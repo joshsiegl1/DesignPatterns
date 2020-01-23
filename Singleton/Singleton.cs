ed65c907e207748e5d09dd5a9701da146ff50feb
Josh <joshsiegl@gmail.com>
2020-01-22 22:20:34 -0500

    added singleton pattern

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace designpatterns.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton UniqueInstance;
+
+        private static readonly object syncLock = new object(); 
+        private Singleton() { }
+
+        public static Singleton getInstance()
+        {
+            lock (syncLock)
+            {
+                if (UniqueInstance == null)
+                {
+                    UniqueInstance = new Singleton();
+                }
+                return UniqueInstance;
+            }
+        }
+    }
+}
